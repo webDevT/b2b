c6a9e3117a65150cc1aaf28c0d53ab4efe03a66d
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-10-22 10:59:30 +0300

    buttons

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,13 @@ $('.slider').slick({
 	prevArrow: $('.prev'),
 nextArrow: $('.next'),
 });
+  // ------start scroll menu---
+$(".button").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+ // ------end scroll menu---
 
 });
